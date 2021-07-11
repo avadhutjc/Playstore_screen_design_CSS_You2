5c31cc50fa3ceaddb4b8117f3990dd8be3c76c94
Avadhutjc <Avadhutjc@gmail.com>
2021-07-11 18:48:28 +0530

    PlayStore Landing Page

---
+++ app/src/main/java/com/masai/a428june_playstore_screen_design_css_you2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.masai.a428june_playstore_screen_design_css_you2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
